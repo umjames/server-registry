8e09c9388365df2726ebab0b34538a2c165dfa46
Michael James <umjames29@gmail.com>
2012-12-26 14:53:55 -0500

    Added ability to see server associated with a category.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def server_display_name(server)
+		if server.ip_address.blank?
+			return server.hostname
+		else
+			if server.hostname.blank?
+				return server.ip_address
+			else
+				return "#{server.hostname} (#{server.ip_address})"
+			end
+		end
+	end
 end
--- app/helpers/servers_helper.rb
+++ app/helpers/servers_helper.rb
@@ -1,13 +1,2 @@
 module ServersHelper
-	def server_display_name(server)
-		if server.ip_address.blank?
-			return server.hostname
-		else
-			if server.hostname.blank?
-				return server.ip_address
-			else
-				return "#{server.hostname} (#{server.ip_address})"
-			end
-		end
-	end
 end
